39e4c20589f61821ec3f6f8d1ee43ce1e202b4f3
CryptoKass <kasscrypto@gmail.com>
2024-12-30 17:07:48 +0000

    add support for big int values

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -5,6 +5,7 @@ import { getBalance, GetBalanceToolDefinition } from "./get_balance.js";
 import { transfer, TransferToolDefinition } from "./transfer.js";
 import { callContract, CallContractToolDefinition } from "./call_contract.js";
 import type { WalletProvider } from "../wallet.js";
+import { bigint } from "zod";
 
 // Creates all tools for the agent
 export const createTools = (agent: WalletProvider) => [
@@ -15,5 +16,12 @@ export const createTools = (agent: WalletProvider) => [
 ];
 
 export const asJson = <T, R>(fn: (params: T) => Promise<R>) => {
-  return async (params: T) => JSON.stringify(await fn(params));
+  return async (params: T) =>
+    JSON.stringify(await fn(params), (k, v) => {
+      // handle bigint values
+      if (v instanceof BigInt) {
+        return v.toString();
+      }
+      return v;
+    });
 };
