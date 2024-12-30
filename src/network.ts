856943d76aa682ca4cbb043126b024b48365b56f
CryptoKass <kasscrypto@gmail.com>
2024-12-30 17:21:16 +0000

    add default network details

--- src/network.ts
+++ src/network.ts
@@ -10,3 +10,19 @@ export interface Network {
 export const makeNetworkProvider = (network: Network) => {
   return new JsonRpcProvider(network.rpcUrl);
 };
+
+export const NETWORKS = {
+  PhoenixMainnet: {
+    name: "LightLink (Phoenix Mainnet)",
+    chainId: 1890,
+    rpcUrl: "https://replicator.phoenix.lightlink.io/rpc/v1",
+    explorerUrl: "https://phoenix.lightlink.io",
+  } as Network,
+
+  PegasusTestnet: {
+    name: "LightLink (Pegasus Testnet)",
+    chainId: 1891,
+    rpcUrl: "https://replicator.pegasus.lightlink.io/rpc/v1",
+    explorerUrl: "https://pegasus.lightlink.io",
+  } as Network,
+};
