563721bed1e9c4f99265584b70c7492d6dfbb975
CryptoKass <kasscrypto@gmail.com>
2025-01-22 16:39:34 +0000

    remove unused tool/default.ts

--- src/tools/default.ts
+++
@@ -1,5 +0,0 @@
-import { Calculator } from "@langchain/community/tools/calculator";
-import type { StructuredToolInterface } from "@langchain/core/tools";
-import { z } from "zod";
-
-type ZodObjectAny = z.ZodObject<any, any, any, any>;
